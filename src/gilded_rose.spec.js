be007112586afd72a9b8d757a4b27a3bac7294a8
Rise Erpelding <rise@heysparkbox.com>
2021-02-01 16:52:53 -0500

    Test: Add working test
    https://sparkbox.atlassian.net/browse/FSA2021-25

--- src/gilded_rose.spec.js
+++ src/gilded_rose.spec.js
@@ -1,10 +1,10 @@
 import { Item, updateQuality } from './gilded_rose';
 
 describe('`updateQuality`', () => {
-  it.todo("Why won't my test pass?", () => {
+  it("Updates the Quality", () => {
     const standardItem = new Item('Haunted Shoe', 10, 10);
     updateQuality([standardItem]);
-    expect(standardItem.sell_in).toBe(4);
+    expect(standardItem.quality).toBe(9);
   });
 
   it.todo('This is a good place for a good test!');
