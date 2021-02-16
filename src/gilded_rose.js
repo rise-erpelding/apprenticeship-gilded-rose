9b77a6b1848c39844a06c3fcd6e8e667f49572fb
Rise Erpelding <rise@heysparkbox.com>
2021-02-16 08:42:49 -0500

    refactor: add default argument to increase/decrease functions

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -22,11 +22,11 @@ const items = [
 updateQuality(items);
 */
 
-function decreaseQuality(currQuality, decAmount) {
+function decreaseQuality(currQuality, decAmount = 1) {
   return currQuality - decAmount;
 }
 
-function increaseQuality(currQuality, incAmount) {
+function increaseQuality(currQuality, incAmount = 1) {
   if (currQuality >= 50) {
     return 50;
   }
@@ -37,7 +37,7 @@ function setQualityToZero() {
   return 0;
 }
 
-function decreaseSellIn(currSellIn, decAmount) {
+function decreaseSellIn(currSellIn, decAmount = 1) {
   return currSellIn - decAmount;
 }
 
@@ -52,9 +52,9 @@ function updateQuality(item) {
      * aged brie increases in quality the older it gets
      * it didn't explicitly say anywhere that aged brie increases in quality double after sell_in is 0, but this is the way it behaved in the original function
      */
-    item.quality = increaseQuality(item.quality, 1);
+    item.quality = increaseQuality(item.quality);
     if (item.sell_in < 0) {
-      item.quality = increaseQuality(item.quality, 1);
+      item.quality = increaseQuality(item.quality);
     }
   }
   else if (item.name.toLowerCase().includes('backstage passes')) {
@@ -64,12 +64,12 @@ function updateQuality(item) {
     * and by 3 when there are 5 days or less
     * but quality drops to 0 after the concert
     */
-    item.quality = increaseQuality(item.quality, 1);
+    item.quality = increaseQuality(item.quality);
     if (item.sell_in < 11) {
-      item.quality = increaseQuality(item.quality, 1);
+      item.quality = increaseQuality(item.quality);
     }
     if (item.sell_in < 6) {
-      item.quality = increaseQuality(item.quality, 1);
+      item.quality = increaseQuality(item.quality);
     }
     if (item.sell_in <= 0) {
       item.quality = setQualityToZero();
@@ -81,10 +81,10 @@ function updateQuality(item) {
   else {
     // general items/standard items/normal items 
     if (item.quality > 0) {
-      item.quality = decreaseQuality(item.quality, 1);
+      item.quality = decreaseQuality(item.quality);
     }
     if (item.sell_in < 0 && item.quality > 0) {
-      item.quality = decreaseQuality(item.quality, 1);
+      item.quality = decreaseQuality(item.quality);
     }
   }
 }
@@ -94,7 +94,7 @@ function updateSellIn(item) {
     return;
   }
   else {
-    item.sell_in = decreaseSellIn(item.sell_in, 1);
+    item.sell_in = decreaseSellIn(item.sell_in);
   }
 }
 
